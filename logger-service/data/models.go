2716b07201a01f14db17a872ab6eb287f5ee6e38
Marc Kelsey <marc.kelsey@textnow.com>
2023-02-01 15:51:13 -0500

    Add logger handler to broker

--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -28,7 +28,7 @@ type Models struct {
 type LogEntry struct {
 	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
 	Name      string    `bson:"name" json:"name"`
-	Data      string    `bson:"name" json:"name"`
+	Data      string    `bson:"data" json:"data"`
 	CreatedAt time.Time `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
 }
